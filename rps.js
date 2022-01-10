c070467b2934de148815290ea77dad5e53d98c79
Evan Hayes <hayesevan7@gmail.com>
2022-01-10 14:40:41 -0500

    added scoring and messages

--- rps.js
+++ rps.js
@@ -13,22 +13,37 @@ function playRound(playerSelection, computerSelection){
         if (playerSelection === computerSelection) {
           return("Tie!")
         }
-        if (
-          (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
-          (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
-          (playerSelection === 'PAPER' && computerSelection === 'ROCK')
-        ) {
-          playerScore++
-          return ("Player wins!")
+        if
+          (playerSelection === 'ROCK' && computerSelection === 'SCISSORS' ||
+          playerSelection === 'SCISSORS' && computerSelection === 'PAPER' ||
+          playerSelection === 'PAPER' && computerSelection === 'ROCK'){
+         
+            playerScore++;
+            return ("Player wins!");
         }
-        if (
-          (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
-          (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
-          (computerSelection === 'PAPER' && playerSelection === 'ROCK')
-        ) {
-          computerScore++
-          return ("Computer wins!")
-        } 
+        if 
+          (computerSelection === 'ROCK' && playerSelection === 'SCISSORS' ||
+          computerSelection === 'SCISSORS' && playerSelection === 'PAPER' ||
+          computerSelection === 'PAPER' && playerSelection === 'ROCK'){ 
 
+            computerScore++;
+            return ("Computer wins!");
+        } 
+}
+
+function game(){
+    if (playerScore === 5 || computerScore === 5){
+        return ("Game is over, someone has scored 5 points!")
+    }
 }
 console.log(playRound(playerSelection, computerSelection));
+console.log(playerScore);
+console.log(computerScore);
+console.log(game());
+
+
+
+
+
+
+
