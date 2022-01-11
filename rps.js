ddaf0cd3337cd5c391aa35fa60d4892feb689dd6
Evan Hayes <hayesevan7@gmail.com>
2022-01-10 20:01:33 -0500

    fixed random number generator

--- rps.js
+++ rps.js
@@ -1,11 +1,13 @@
-function computerPlay(){
-    let choices = ["ROCK", "PAPER", "SCISSORS"]
-    let randomIndex = Math.floor(Math.random()*choices.length);
-    return choices[randomIndex];
+function computerPlay() {
+    let random = Math.random();
+    if (random <= 0.3333) {
+        return "PAPER";
+    } else if (random >= 0.6666) {
+        return "ROCK";
+    } else {
+        return "SCISSORS";
+    }
 }
-
-let computerSelection = computerPlay();
-let playerSelection = prompt("Enter Rock, Paper, or Scissors").toUpperCase();
 let playerScore = 0;
 let computerScore = 0;
 
@@ -26,19 +28,38 @@ function playRound(playerSelection, computerSelection){
           computerSelection === 'SCISSORS' && playerSelection === 'PAPER' ||
           computerSelection === 'PAPER' && playerSelection === 'ROCK'){ 
 
-            computerScore++;
+            computerScore++
             return ("Computer wins!");
         } 
 }
 
+let i =0;
 function game(){
-    if (playerScore === 5 || computerScore === 5){
-        return ("Game is over, someone has scored 5 points!")
+    let computerSelection = computerPlay()
+    let playerSelection = prompt("Enter Rock, Paper, or Scissors").toUpperCase();
+    console.log(playRound(playerSelection, computerSelection))
+    console.log("your score = " + playerScore);
+    console.log("Computer's score = " + computerScore);
+    i++;
+    if (i !== 5) {
+        game();
+    } else {
+        alert("Game Over=> User("+playerScore+") vs Computer("+computerScore+")");
     }
 }
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerScore);
-console.log(computerScore);
+
+game();
+
+
+
+
+
+
+
+
+
+
+
 
 
 
